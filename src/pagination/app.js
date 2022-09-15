cdf2837056278d32522c905da26ee9d35e3b6e65
Manish102866 <109049316+Manish102866@users.noreply.github.com>
2022-09-15 18:54:57 +0900

    Added pagination code

---
+++ src/pagination/app.js
@@ -0,0 +1,40 @@
+import React, { useState, useMemo } from 'react';
+import Pagination from '../pagination';
+import data from './db/media-db.json';
+import './app.scss';
+
+let PageSize = 20;
+
+export default function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+    const currentMediaData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return data.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+
+  return (
+    <>
+    <div className='container'>
+        {currentMediaData.map(item => {
+            return (
+                <ul>
+                    <li><img src={ item.img_path } alt=""></img></li>
+                    <li><span>Image Name:</span> <label>{item.img_name}</label></li>
+                    <li><span>Description:</span> <label className='des' >{item.description}</label></li>
+                    <li><span>Date:</span> <label>{item.date}</label></li>
+                </ul>
+            );
+          })}
+    </div>
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={data.length}
+        pageSize={PageSize}
+        onPageChange={page => setCurrentPage(page)}
+      />
+    </>
+  );
+}
